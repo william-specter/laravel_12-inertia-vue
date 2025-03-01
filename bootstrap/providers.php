10c59fa58d9e4c66e76f1edf652f26a9b65b6dc4
William-Specter <willyburrel@gmail.com>
2025-02-28 23:34:25 -0500

    first commit

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\FolioServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
+];
